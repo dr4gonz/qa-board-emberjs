7d3adefe945873cf9eb40474ff333aef049f5f48
Matthew <mreyez@gmail.com>
2016-08-19 09:15:57 -0700

    refactor index to display questions by date descending (newest first)

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.query('question', {
+        orderBy: 'date'
+      }).then((response) => {
+        return response.toArray().reverse();
+      }),
+    });
   }
 });
