7bf9e2bae720c60af1bb50c130291a3a70b26c53
Matthew <mreyez@gmail.com>
2016-08-19 09:12:56 -0700

    add new-question route and route to individual questions

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('contact');
   this.route('about');
   this.route('new-question');
+  this.route('question', {path: './question/:question_id'});
 });
 
 export default Router;
--- app/routes/new-question.js
+++ app/routes/new-question.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createPost(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+  }
 });
